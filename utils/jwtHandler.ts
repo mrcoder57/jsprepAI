ae174c3cd0c53e3f74b820d62d9c6d72886fa582
mrcoder57 <tiwariji2300@gmail.com>
2024-12-15 17:05:19 +0530

    feature:add verfiy token

--- utils/jwtHandler.ts
+++ utils/jwtHandler.ts
@@ -3,6 +3,9 @@ import jwt from "jsonwebtoken";
 const JWT_SECRET = process.env.jwt_secret!;
 export const generateToken = (user: any) => {
  
+  if(!user||!user.id){
+    throw new Error('User is not defined or userId is not defined');
+  }
   const payload = {
     userId: user.id,
     email: user.email,
@@ -13,6 +16,23 @@ export const generateToken = (user: any) => {
   return token;
 };
 interface TokenPayload {
-  userId: string;
+  id: string;
   email: string;
+}
+
+
+
+export function verifyToken(token: string): TokenPayload {
+  if (!JWT_SECRET) {
+    throw new Error('JWT secret is not defined in environment variables.');
+  }
+
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET) as TokenPayload;
+    // console.log('Decoded token:', decoded);  // Log the decoded token
+    return decoded;
+  } catch (error) {
+    console.error('Error verifying token:', error);
+    throw new Error('Invalid or expired token');
+  }
 }
